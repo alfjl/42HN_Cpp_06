7499ab1da741a8da1e3b4396e399973d2f2ac23e
alanghan <alanghan@student.42heilbronn.de>
2022-03-25 15:46:17 +0100

    Add *next test to main.cpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,6 +6,7 @@ typedef struct	s_data
 	int			ageInYears;
 	float		heightInMeters;
 	double		weightInKilos;
+	s_data		*next;
 }		Data;
 
 uintptr_t	serialize(Data *ptr)
@@ -21,6 +22,7 @@ Data		*deserialize(uintptr_t raw)
 int	main()
 {
 	Data		dataStruct;
+	Data		dataStruct2;
 	Data		*deserializedDataStruct;
 	uintptr_t	pointerToDataStruct;
 
@@ -28,6 +30,13 @@ int	main()
 	dataStruct.ageInYears = 30;
 	dataStruct.heightInMeters = 1.83f;
 	dataStruct.weightInKilos = 85.9;
+	dataStruct.next = &dataStruct2;
+
+	dataStruct2.name = "DataStruct2";
+	dataStruct2.ageInYears = 100;
+	dataStruct2.heightInMeters = 2.22f;
+	dataStruct2.weightInKilos = 44.4;
+	dataStruct2.next = nullptr;
 
 	pointerToDataStruct = serialize(&dataStruct);
 	deserializedDataStruct = deserialize(pointerToDataStruct);
@@ -39,9 +48,13 @@ int	main()
 		<< "\ndataStruct.heightInMeters: " << dataStruct.heightInMeters << " / deserialized: " << deserializedDataStruct->heightInMeters
 		<< "\ndataStruct.weightInKilos: " << dataStruct.weightInKilos << " / deserialized: " << deserializedDataStruct->weightInKilos
 		<< std::endl;
-
-
-
+	std::cout << "\n########## TESTS *NEXT ##########" << std::endl;
+	std::cout
+		<< "dataStruct->next.name: " << (*dataStruct.next).name << " / deserialized: " << deserializedDataStruct->next->name
+		<< "\ndataStruct->next.ageInYears: " << (*dataStruct.next).ageInYears << " / deserialized: " << deserializedDataStruct->next->ageInYears
+		<< "\ndataStruct->next.heightInMeters: " << (*dataStruct.next).heightInMeters << " / deserialized: " << deserializedDataStruct->next->heightInMeters
+		<< "\ndataStruct->next.weightInKilos: " << (*dataStruct.next).weightInKilos << " / deserialized: " << deserializedDataStruct->next->weightInKilos
+		<< std::endl;
 
 	return (0);
 }
